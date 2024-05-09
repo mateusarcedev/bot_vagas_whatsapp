2be8df096002dabbe4389a7e820b94a34ee9082d
Mateus Arce <arcemateuss@gmail.com>
2024-05-09 16:25:34 -0400

    changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bot_vagas_whatsapp
+
+- [] Listar Vagas a cada 1 hora
+- [] Exibir menu de vagas por área
+- [] Salvar contatos que desejam receber vagas em um banco de dados
--- src/main.js
+++ src/main.js
@@ -1,67 +1,82 @@
-const qrcode = require('qrcode-terminal');
-const { Client } = require('whatsapp-web.js');
-const axios = require('axios');
+const qrcode = require("qrcode-terminal");
+const { Client } = require("whatsapp-web.js");
+const axios = require("axios");
 
 const client = new Client();
 
-client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-    console.log('Cliente está pronto!');
+client.on("ready", () => {
+  console.log("Cliente está pronto!");
 });
 
-client.on('message', async (message) => {
-    const messageBody = message.body.toLowerCase(); 
-    switch(messageBody) {
-        case '1':
-            message.reply('Resposta para o caso 1');
-            break;
-        case 'vagas_back':
-            await sendBackJobsToWhatsApp(message.from)
-            break;
-        case 'vagas_front':
-            await sendFrontJobsToWhatsApp(message.from);
-            break;
-        default:
-            message.reply('Mensagem não reconhecida.');
-            break;
-    }
+client.on("message", async (message) => {
+  const messageBody = message.body.toLowerCase();
+
+  if (messageBody == "oi" || messageBody == "menu") {
+    const menu = `
+🌟 *SEJA BEM-VINDO AO BOT DE VAGAS* 🌟
+
+Escolha uma opção:
+
+1️⃣ - VAGAS ANDROID
+2️⃣ - VAGAS BACKEND
+3️⃣ - VAGAS FRONTEND
+4️⃣ - VAGAS JAVA
+5️⃣ - VAGAS QA
+6️⃣ - VAGAS REACT
+7️⃣ - VAGAS UX
+8️⃣ - VAGAS JR OU ESTÁGIO`;
+
+    message.reply(menu);
+  }
+
+  switch (messageBody) {
+    case "1":
+      await sendJobsToWhatsApp(message.from, "androiddevbr/vagas");
+      break;
+    case "2":
+      androiddevbr / vagas;
+      await sendJobsToWhatsApp(message.from, "backend-br/vagas");
+      break;
+    case "3":
+      await sendJobsToWhatsApp(message.from, "frontendbr/vagas");
+    case "4":
+      await sendJobsToWhatsApp(message.from, "soujava/vagas-java");
+    case "5":
+      await sendJobsToWhatsApp(message.from, "qa-brasil/vagas");
+    case "6":
+      await sendJobsToWhatsApp(message.from, "react-brasil/vagas");
+    case "7":
+      await sendJobsToWhatsApp(message.from, "remotejobsbr/design-ux-vagas");
+    case "8":
+      await sendJobsToWhatsApp(
+        message.from,
+        "alinebastos/vagas-junior-estagio"
+      );
+    default:
+      break;
+  }
 });
 
 client.initialize();
 
-async function sendFrontJobsToWhatsApp(sender) {
-    try {
-        const response = await axios.get('https://api.github.com/repos/frontendbr/vagas/issues');
-        const issues = response.data;
-        let issuesText = '';
+async function sendJobsToWhatsApp(sender, repository) {
+  try {
+    const response = await axios.get(
+      `https://api.github.com/repos/${repository}/issues`
+    );
+    const issues = response.data;
+    let issuesText = "";
 
-        issues.forEach(issue => {
-            issuesText += `*${issue.title}*\n${issue.html_url}\n\n`;
-        });
+    issues.forEach((issue) => {
+      issuesText += `*${issue.title}*\n${issue.html_url}\n\n`;
+    });
 
-        await client.sendMessage(sender, issuesText);
-    } catch (error) {
-        console.error('Erro ao enviar issues para o WhatsApp:', error);
-    }
-}
-
-
-
-async function sendBackJobsToWhatsApp(sender) {
-    try {
-        const response = await axios.get('https://api.github.com/repos/backend-br/vagas/issues')
-        const issues = response.data
-        let issuesText = ''
-
-        issues.forEach(issue => {
-            issuesText += `*${issue.title}*\n${issue.html_url}\n\n`;
-        })
-
-        await client.sendMessage(sender, issuesText)
-    } catch(error) {
-        console.error('Erro ao enviar issues para o WhatsApp:', error);
-    }
+    await client.sendMessage(sender, issuesText);
+  } catch (error) {
+    console.error("Erro ao enviar issues para o WhatsApp:", error);
+  }
 }
